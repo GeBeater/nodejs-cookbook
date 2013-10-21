2f54d3607589c1deefed5d40a201e7c99575e8e5
GeBeater <info@gebeat.com>
2013-10-21 21:46:53 +0200

    version update * change nodejs version from 0.10.15 to 0.10.21 * use npm version 1.3.11 instead of 1.3.5

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,12 +24,12 @@ case node['platform_family']
     default['nodejs']['install_method'] = 'source'
 end
 
-default['nodejs']['version'] = '0.10.15'
-default['nodejs']['checksum'] = '87345ab3b96aa02c5250d7b5ae1d80e620e8ae2a7f509f7fa18c4aaa340953e8'
-default['nodejs']['checksum_linux_x64'] = '0b5191748a91b1c49947fef6b143f3e5e5633c9381a31aaa467e7c80efafb6e9'
-default['nodejs']['checksum_linux_x86'] = '7ff9fb6aa19a5269a5a2f7a770040b8cd3c3b528a9c7c07da5da31c0d6dfde4d'
+default['nodejs']['version'] = '0.10.21'
+default['nodejs']['checksum'] = '7c125bf22c1756064f2a68310d4822f77c8134ce178b2faa6155671a8124140d'
+default['nodejs']['checksum_linux_x64'] = '2791efef0a1e9a9231b937e55e5b783146e23291bca59a65092f8340eb7c87c8'
+default['nodejs']['checksum_linux_x86'] = 'c0ed641961a5c5a4602b1316c3d3ed12b3ac330cc18abf3fb980f0b897b5b96b'
 default['nodejs']['dir'] = '/usr/local'
-default['nodejs']['npm'] = '1.3.5'
+default['nodejs']['npm'] = '1.3.11'
 default['nodejs']['src_url'] = "http://nodejs.org/dist"
 default['nodejs']['make_threads'] = node['cpu'] ? node['cpu']['total'].to_i : 2
 default['nodejs']['check_sha'] = true
